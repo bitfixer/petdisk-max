a1dc27a2f740e7ab3e763dce7d0b767dd5829d3c
Michael Hill <bitfixer@bitfixer.com>
2021-01-13 11:34:16 -0800

    added Makefile and web server

---
+++ Makefile
@@ -0,0 +1,130 @@
+# Name: Makefile
+# Author: <insert your name here>
+# Copyright: <insert your copyright message here>
+# License: <insert your license reference here>
+
+DEVICE     = atmega644p
+CLOCK      = 8000000
+PROGRAMMER = -c linuxspi -P /dev/spidev0.0
+SRCDIR     = src
+OBJECTS    = $(SRCDIR)/EspConn.o\
+$(SRCDIR)/Serial.o\
+$(SRCDIR)/EspHttp.o\
+$(SRCDIR)/SD_routines.o\
+$(SRCDIR)/SPI_routines.o\
+$(SRCDIR)/FAT32.o\
+$(SRCDIR)/IEEE488.o\
+$(SRCDIR)/NetworkDataSource.o
+
+FUSES 		= -U lfuse:w:0xc2:m -U hfuse:w:0xda:m -U efuse:w:0xff:m -U lock:w:0xEF:m
+
+# For computing fuse byte values for other devices and options see
+# the fuse bit calculator at http://www.engbedded.com/fusecalc/
+
+
+# Tune the lines below only if you know what you are doing:
+
+AVRDUDE = avrdude $(PROGRAMMER) -p $(DEVICE) -V
+COMPILE = avr-g++ -Wall -Os -DF_CPU=$(CLOCK) -mmcu=$(DEVICE) -std=gnu99 $(INCLUDE)
+COMPILECPP = avr-g++ -Wall -Os -DF_CPU=$(CLOCK) -mmcu=$(DEVICE) -std=c++11 $(INCLUDE)
+
+BOOTLOADER_ADDR_324_H = 0x7000
+BOOTLOADER_ADDR_644_H = 0xF000
+BOOTLOADER_ADDR_324_D = 28672
+BOOTLOADER_ADDR_644_D = 61440
+
+# symbolic targets:
+all:	petdisk.hex
+
+.c.o:
+	$(COMPILE) -c $< -o $@
+
+.cpp.o:
+	$(COMPILECPP) -c $< -o $@
+
+.S.o:
+	$(COMPILE) -x assembler-with-cpp -c $< -o $@
+# "-x assembler-with-cpp" should not be necessary since this is the default
+# file type for the .S (with capital S) extension. However, upper case
+# characters are not always preserved on Windows. To ensure WinAVR
+# compatibility define the file type manually.
+
+.c.s:
+	$(COMPILE) -S $< -o $@
+
+%.flash:
+	$(AVRDUDE) -U flash:w:$*.hex
+
+%.flashbin:
+	$(AVRDUDE) -U flash:w:$*.bin -V
+
+fuse:
+	$(AVRDUDE) -b115200 $(FUSES)
+
+%.program: %.hex
+	make progenable
+	sudo make $*.install
+	make progdisable
+
+%.programbin: %.bin
+	make progenable
+	sudo make $*.installbin
+	make progdisable
+
+%.install: %.hex
+	make fuse
+	make $*.flash
+
+%.installbin: %.bin
+	make fuse
+	make $*.flashbin
+
+progenable:
+	gpio mode 12 alt0
+	gpio mode 13 alt0
+	gpio mode 14 alt0
+	gpio mode 6 in
+
+progdisable:
+	gpio mode 12 in
+	gpio mode 13 in
+	gpio mode 14 in
+	gpio mode 6 out
+	gpio write 6 0
+	gpio write 6 1
+
+clean:
+	rm -f *.hex *.elf *.o *.bin
+	rm -f $(SRCDIR)/*.o
+
+$(SRCDIR)/githash.h:
+	echo const unsigned char _hash[] PROGMEM = \"$(shell git rev-parse --short HEAD | tr [:lower:] [:upper:])\"\; > $@
+
+# file targets:
+%.elf: $(SRCDIR)/githash.h %.o $(OBJECTS)
+	$(COMPILECPP) -o $*.elf $*.o $(OBJECTS)
+	rm $(SRCDIR)/githash.h
+
+%.hex: %.elf
+	rm -f $*.heximage
+	avr-objcopy -j .text -j .data -O ihex $*.elf $*.hex
+	avr-size --format=avr --mcu=$(DEVICE) $*.elf
+# If you have an EEPROM section, you must also create a hex file for the
+# EEPROM and add it to the "flassh" target.
+
+%.bin: %.elf
+	rm -f $*.bin
+	avr-objcopy -j .text -j .data -O binary $*.elf $*.bin
+	avr-size --format=avr --mcu=$(DEVICE) $*.elf
+
+# Targets for code debugging and analysis:
+disasm:	%.elf
+	avr-objdump -d $*.elf
+
+bootloader.elf: bootloader.o SPI_routines.o SD_routines.o FAT32_tiny.o
+	$(COMPILECPP) -Ttext=$(BOOTLOADER_ADDR_644_H) -o bootloader.elf bootloader.o SPI_routines.o SD_routines.o FAT32_tiny.o
+
+# pad the end of the main program with zeros, leaving enough room for the bootloader
+petdisk_and_bootloader.bin: petdisk.bin bootloader.bin
+	dd if=/dev/zero bs=1 count=$(shell expr $(BOOTLOADER_ADDR_644_D) - $(shell stat --format="%s" petdisk.bin)) >> petdisk.bin
+	cat petdisk.bin bootloader.bin > petdisk_and_bootloader.bin
---
+++ www/petdisk.php
@@ -0,0 +1,167 @@
+<?php
+
+function fileExists($fileName, $caseSensitive = true) 
+{
+    if (file_exists($fileName)) 
+    {
+        return $fileName;
+    }
+
+    if ($caseSensitive)
+    {
+        return false;
+    }
+
+    // Handle case insensitive requests            
+    $directoryName = dirname($fileName);
+    $fileArray = glob($directoryName . '/*', GLOB_NOSORT);
+    $fileNameLowerCase = strtolower($fileName);
+    foreach ($fileArray as $file) 
+    {
+        if (strtolower($file) == $fileNameLowerCase) 
+        {
+            return $file;
+        }
+    }
+    return false;
+}
+
+header_remove("X-Powered-By");
+
+$verb = $_SERVER['REQUEST_METHOD'];
+if ($verb == "GET")
+{
+    $fname = "./".$_GET['file'];
+    $file = fileExists($fname, false);
+    if ($file)
+    {
+        $len = $_GET['l'];
+        if ($len == 1)
+        {
+            $fs = filesize($file);
+            $resp = $fs."\r\n";
+            header('Content-Length: '.strlen($resp));
+            header('Content-Type: application/octet-stream');
+            echo $resp;
+            flush();
+        }
+        // directory request
+        else if ($_GET['d'] == 1)
+        {
+            // directory
+            // check for the index of the page requested.
+
+            $page = -1;
+            if (array_key_exists('p', $_GET))
+            {
+                $page = $_GET['p'];
+            }
+
+            $dir = ".";
+            $files = scandir($dir);
+            $filelist = "";
+            $pagesize = 0;
+            $max_page_size = 512;
+            $dir_pages = array();
+            $curr_page = 0;
+            foreach ($files as $file)
+            {
+                $file_parts = pathinfo($file);
+                if (strtolower($file_parts['extension']) == "prg")
+                {
+                    $newentry = strtoupper($file)."\n";
+                    if ($page >= 0 && $pagesize + strlen($newentry) >= $max_page_size)
+                    {
+                        // end this page and make a new page
+                        $dir_pages[$curr_page] = $filelist."\n";
+                        $curr_page++;
+                        $filelist = "";
+                    }
+                    $filelist = $filelist.strtoupper($file)."\n";
+                }
+
+                $pagesize = strlen($filelist);
+            }
+
+            // last page
+            $dir_pages[$curr_page] = $filelist."\n";
+            
+            $respbody = "\n";
+            if ($page == -1)
+            {
+                $respbody = $dir_pages[0];
+            }
+            else
+            {
+                if ($page <= $curr_page)
+                {
+                    $respbody = $dir_pages[$page];
+                }
+            }
+
+            header('Content-Length: '.strlen($respbody));
+            header('Content-Type: application/octet-stream');
+            echo $respbody;
+            flush();
+        }
+        else
+        {
+            $start = $_GET['s'];
+            $end = $_GET['e'];
+            $fp = fopen($file, "r");
+
+            if ($fp)
+            {
+                fseek($fp, $start, SEEK_SET);
+                $contents = fread($fp, $end-$start);
+                $content_length = $end-$start;
+                header('Content-Length: '.$content_length);
+                header('Content-Type: application/octet-stream');
+                echo $contents;
+                flush();
+            }
+        }
+    }
+}
+else if ($verb == "PUT")
+{
+    $fname = $_GET['f'];
+    $new = $_GET['n'];
+
+    // read put data
+    $putdata = file_get_contents("php://input");
+    
+    $full_fname = "";
+    if ($fname)
+    {
+        $full_fname = "./".$fname;
+    }
+        
+    $exists = false;
+    if ($full_fname != "")
+    {
+        $exists = file_exists($full_fname);
+    }
+
+    // remove existing file if new specified
+    $file_contents = "";
+    if ($exists == true && $new == 1)
+    {
+        unlink($file);
+        $exists = false;
+    } 
+    
+    if ($exists == true) 
+    {
+        $file_contents = file_get_contents($full_fname);
+    }
+
+    // append new data
+    $file_contents = $file_contents . $putdata;
+    // rewrite file
+    file_put_contents($full_fname, $file_contents);
+    
+}
+
+
+?>
