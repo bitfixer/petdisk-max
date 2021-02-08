2adf7b8d0e91fbcde2dda51d025c8e624e9aecbc
Michael Hill <bitfixer@bitfixer.com>
2021-02-07 23:10:08 -0800

    moved filename processing

--- src/petdisk.cpp
+++ src/petdisk.cpp
@@ -692,6 +692,21 @@ DataSource* PETdisk::getDataSource(unsigned char id)
     return _dataSources[id-MIN_DEVICE_ID];
 }
 
+unsigned char processFilename(unsigned char* filename, unsigned char length)
+{
+    unsigned char drive_separator = ':';
+    unsigned char* sepptr = (unsigned char*)memmem(filename, length, &drive_separator, 1);
+    if (sepptr)
+    {
+        // found a drive separator
+        int seplen = sepptr - filename + 1;
+        memmove(filename, sepptr + 1, length - seplen);
+        length -= seplen;
+    }
+
+    return length;
+}
+
 int main(void)
 {
     _bufferSize = 0;
@@ -893,12 +908,8 @@ int main(void)
                         logSerial.transmitString((char*)progname);
                         logSerial.transmitString("\r\n");
 
-                        // check for DLOAD command, will contain : character
-                        if (progname[1] == ':')
-                        {
-                            filename_position -= 2;
-                            memmove(progname, &progname[2], filename_position);
-                        }
+
+                        filename_position = processFilename(progname, filename_position);
 
                         // copy the PRG file extension onto the end of the file name
                         pgm_memcpy(&progname[filename_position], (unsigned char *)_fileExtension, 5);
@@ -912,91 +923,7 @@ int main(void)
                     }
                 }
             }
-
         }
-
-        /*
-        if (filenotfound == 1)
-        {
-            filenotfound = 0;
-            ieee.unlisten();
-        }
-        else if ((rdchar == 0xF0 || rdchar == 0xF1) && ieee.atn_is_low())
-        {
-            getting_filename = 1;
-            memset(progname, 0, 255);
-            
-            if (rdchar == 0xF1)
-            {
-                savefile = 1;
-            }
-            else
-            {
-                savefile = 0;
-            }
-        }
-        else if (getting_filename == 1) // reading bytes of a filename
-        {
-            // add character to filename
-            progname[filename_position++] = rdchar;
-            progname[filename_position] = 0;
-
-            // is this the last character?
-            if (ieee.eoi_is_low())
-            {
-                if (progname[0] == '$') // directory request
-                {
-                    getting_filename = 0;
-                    filename_position = 0;
-
-                    if (!dataSource->isInitialized())
-                    {
-                        filenotfound = 1;
-                    }
-                }
-                else // file load command
-                {
-                    logSerial.transmitString("got filename:\r\n");
-                    logSerial.transmitString((char*)progname);
-                    logSerial.transmitString("\r\n");
-
-                    // check for DLOAD command, will contain : character
-                    if (progname[1] == ':')
-                    {
-                        filename_position -= 2;
-                        memmove(progname, &progname[2], filename_position);
-                    }
-
-                    // copy the PRG file extension onto the end of the file name
-                    pgm_memcpy(&progname[filename_position], (unsigned char *)_fileExtension, 5);
-
-                    // have the full filename now
-                    getting_filename = 0;
-                    //logger.log((char*)progname);
-                    //logger.log("**\r\n");
-
-                    logSerial.transmitString((char*)progname);
-                    logSerial.transmitString((char*)"\r\n");
-
-                    filename_position = 0;
-                    gotname = 1;
-                }
-            }
-        }
-        else if (rdchar == 0x60 && ieee.atn_is_low())
-        {
-            //logger.log("checking for dir\r\n");
-            if (progname[0] == '$')
-            {
-                // copy the directory header
-                pgm_memcpy((unsigned char *)_buffer, (unsigned char *)_dirHeader, 7);
-                
-                // print directory title
-                pgm_memcpy((unsigned char *)&_buffer[7], (unsigned char *)_versionString, 24);
-                _buffer[31] = 0x00;
-            }
-        }
-        */
 
         // signal that we are done handling this byte
         ieee.acknowledge_bus_byte();
@@ -1040,7 +967,7 @@ int main(void)
         ieee.signal_ready_for_data();
 
         // LOAD requested
-        if (rdchar == 0x60 && ieee.atn_is_low())
+        if (rdchar == PET_OPEN_IO_ADDR && ieee.atn_is_low())
         {
             // starting LOAD sequence
             if (filenotfound == 0)
@@ -1093,18 +1020,6 @@ int main(void)
 
     while(1) {}
     return 0;
-}
-
-unsigned char processFilename(unsigned char* filename, unsigned char length)
-{
-    unsigned char drive_separator = ':';
-    unsigned char* sepptr = (unsigned char*)memmem(filename, length, &drive_separator, 1);
-    if (sepptr)
-    {
-        // found a drive separator
-        int seplen = sepptr - filename + 1;
-        memmove(filename, sepptr + 1, length - seplen);
-    }
 }
 
 // can you make the interrupt handler self contained?
