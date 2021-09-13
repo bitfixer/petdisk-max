6b0ffe537f2e31663478138bd28a76943fc460d9
Michael Hill <mhill@twitter.com>
2021-09-13 15:25:16 -0700

    moved setting unused sectors after directory check

--- src/FAT32.cpp
+++ src/FAT32.cpp
@@ -160,12 +160,12 @@ unsigned char FAT32::getBootSectorData (void)
     struct partitionInfo_Structure *partition;
     unsigned long dataSectors;
 
-    _unusedSectors = 0;
-
     if (_rootCluster != 0 && _currentDirectoryCluster != _rootCluster)
     {
         return 0;
     }
+
+    _unusedSectors = 0;
 
     _sd->readSingleBlock(0, _FatBuffer);
     bpb = (struct BS_Structure *)_FatBuffer;
