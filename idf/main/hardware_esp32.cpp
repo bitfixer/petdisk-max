b75835d40c127294d33330cd255ad4423e903093
Michael Hill <bitfixer@bitfixer.com>
2025-04-25 23:24:34 -0700

    Revert "simplify atn wait"
    This reverts commit 71fb65469f8b745354c8b24662cafc46ba5d572d.

--- idf/main/hardware_esp32.cpp
+++ idf/main/hardware_esp32.cpp
@@ -641,12 +641,15 @@ void nvs_set_int(const char* key, int32_t val) {
     }
 }
 
-bool atn_low = false;
+static QueueHandle_t atn_queue = NULL;
+static bool b;
+
 static void IRAM_ATTR gpio_isr_handler(void* arg) {
-    atn_low = true;
+    xQueueSendFromISR(atn_queue, (void*)&b, NULL);
 }
 
 void setup_atn_interrupt() {
+    atn_queue = xQueueCreate(1, sizeof(bool));
     // set up ATN as interrupt
     esp_err_t err = gpio_set_intr_type((gpio_num_t)ATN_PIN, GPIO_INTR_NEGEDGE);
     if (err != ESP_OK) {
@@ -657,8 +660,6 @@ void setup_atn_interrupt() {
 }
 
 void wait_atn_isr() {
-    while (!atn_low) {
-        hDelayMs(1);
-    }
-    atn_low = false;
+    bool t;
+    while (xQueueReceive(atn_queue, &t, portMAX_DELAY) != pdTRUE) {}
 }
